cedd3109dcd73603b55a8b18d70e7106e85bd5d9
xuquan <greg_xu@flyingnets.com>
2023-02-24 13:40:41 +0800

    a

--- Automatic-ClientIP-NSG/storageaccount.tf
+++ Automatic-ClientIP-NSG/storageaccount.tf
@@ -5,11 +5,11 @@ resource "azurerm_storage_account" "storage_account_test" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  # network_rules {
-  #     default_action = "Deny"
-  #     ip_rules = ["${chomp(data.http.clientip.response_body)}/30"]
-  #     virtual_network_subnet_ids = [azurerm_subnet.poctest_subnet01.id]
-  # }
+  network_rules {
+      default_action = "Deny"
+      ip_rules = ["${chomp(data.http.clientip.response_body)}/30"]
+      virtual_network_subnet_ids = [azurerm_subnet.poctest_subnet01.id]
+  }
 
   tags = {
     Environment = var.environment_tag
