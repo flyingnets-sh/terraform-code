2745bec979e8f4715706910fba9cc6dc6098a994
xuquan <greg_xu@flyingnets.com>
2023-02-24 14:15:38 +0800

    add

--- Automatic-ClientIP/storageaccount.tf
+++ Automatic-ClientIP/storageaccount.tf
@@ -7,7 +7,8 @@ resource "azurerm_storage_account" "storage_account_test" {
 
   network_rules {
       default_action = "Deny"
-      ip_rules = ["${chomp(data.http.clientip.response_body)}/32"]
+      #Error: "network_rules.0.ip_rules.0" must start with IPV4 address and/or slash, number of bits (0-30) as prefix. Example: 23.45.1.0/30.
+      ip_rules = ["${chomp(data.http.clientip.response_body)}/30"]
       virtual_network_subnet_ids = [azurerm_subnet.poctest_subnet01.id]
   }
 
